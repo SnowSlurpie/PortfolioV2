0abed8a075b03de4b4dbe994a01e96313b3d2cab
Alex <alexjrunning@gmail.com>
2023-11-03 12:05:49 -0500

    updated tempalte and added contact button

--- AngularPortfolio/src/app/app-routing.module.ts
+++ AngularPortfolio/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' }, // redirects to '/about' when the path is empty
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
