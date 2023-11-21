52f140b55fad2ab4cb02decf6e778a53c3aa0f46
Alex <alexjrunning@gmail.com>
2023-11-20 20:05:31 -0600

    finished skeleton for experience and started project card backgrounds

--- AngularPortfolio/src/app/projects/projects.component.ts
+++ AngularPortfolio/src/app/projects/projects.component.ts
@@ -10,9 +10,15 @@ export class ProjectComponent {
   projects = [
     {title: 'Buddy By Daylight',
       description: 'A web-based program inspired by the game Dead By Daylight that allows you to connect with your friends and have fun. ',
-      link: 'https://github.com/SnowSlurpie/Frontend_SBA'},
+      link: 'https://github.com/SnowSlurpie/Frontend_SBA',
+      imageUrl:'https://deadbydaylight.com/static/a5cdc5db3804f817df112184c81f5ead/93f85/dbd-gallery-background.jpg'
+    },
+    {title: 'Jessie\'s List',
+      description: 'An app that allows parents to teach their children the value of working hard and learning responsibility by creating a to-do list. ',
+      link: 'https://github.com/thetinaest/chores-app',
+       //imageUrl:test.com
+    },
 
-    // {title: 'Project 2', description: 'Description for Project 2', link: 'https://link-to-project-2.com'},
 // add more projects here
   ];
 }
