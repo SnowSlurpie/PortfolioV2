568160e5f5feddbf0d4c2e57a99e2b192a37120c
Alex <alexjrunning@gmail.com>
2023-11-27 19:47:24 -0600

    added variable for color in ts

--- AngularPortfolio/src/app/projects/projects.component.ts
+++ AngularPortfolio/src/app/projects/projects.component.ts
@@ -11,7 +11,8 @@ export class ProjectComponent {
     {title: 'Buddy By Daylight',
       description: 'A web-based program inspired by the game Dead By Daylight that allows you to connect with your friends and have fun. ',
       link: 'https://github.com/SnowSlurpie/Frontend_SBA',
-      imageUrl:'https://deadbydaylight.com/static/a5cdc5db3804f817df112184c81f5ead/93f85/dbd-gallery-background.jpg'
+      imageUrl:'https://deadbydaylight.com/static/a5cdc5db3804f817df112184c81f5ead/93f85/dbd-gallery-background.jpg',
+      color: 'white'
     },
     {title: 'Jessie\'s List',
       description: 'An app that allows parents to teach their children the value of working hard and learning responsibility by creating a to-do list. ',
